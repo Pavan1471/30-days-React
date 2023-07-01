574635dea379148adc94cc75ad7754a78a163e2e
pavankumar <puttupavankumar@gmail.com>
2023-07-01 07:31:13 +0530

    modified:   src/App.css 	modified:   src/Map.js 	new file:   src/fruit.png 	modified:   src/index.js

--- src/Map.js
+++ src/Map.js
@@ -1,37 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import fruit from './fruit.png'
+// import fruits from './data.js';
 
-const applicants = [ {
-    name: 'Joe', work: 'freelance-developer',
-    blogs: '54', websites: '32',
-    hackathons: '6', location: 'morocco', id: '0',
+const fruits = [{
+  name:"orange",image:"https://w7.pngwing.com/pngs/187/615/png-transparent-orange-fruit-orange-tangerine-orange-natural-foods-food-citrus-thumbnail.png",taste:"sour",
   },
   {
-    name: 'janet', work: 'fullstack-developer', 
-    blogs: '34', websites: '12', 
-    hackathons: '8', location: 'Mozambique', id: '1',
+  name:"apple",image:"https://static.libertyprim.com/files/familles/pomme-large.jpg?1569271834",taste:"sweet",
   },
-
-];
+  
+  {
+  name:"mango",image:"https://w7.pngwing.com/pngs/790/811/png-transparent-sliced-mangoe-juice-mango-alphonso-fruit-flavor-mango-food-dried-fruit-tropical-fruit-thumbnail.png",taste:"pulpy",
+  },
+  ]
 
 function Map(){
-    return(
-<>
-        {
-          // Type - 1 of mapping
-        // applicants.map(function(data) {
-        //     return (
-        //       <div>
-        //         Applicant name:  {data.name}
-        //       </div>
-        //     )
-        //   })
 
-        // mapping Type -2 using arrow functions
-        applicants.map(data=>
+  const [search,setSearch] = useState("");
+  // const 
+
+    return(
+
+<>
+
+<center>
+  <h1>Search fruits</h1>
+
+  <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/> <br/>
+  
+        
+</center>
+     {
+// Type - 1 of mapping
+        fruits.filter(data=>data.name.toUpperCase().includes(search.toUpperCase())).map(function(data) {
+            return (
+              <div className='container'>
+
+          <img src={data.image}></img> 
           <div>{data.name}</div>
-        )
+          <div>{data.taste}</div>
+           
+              </div>
+              // </br>
+            )
+          })
         }
 </>
     )
